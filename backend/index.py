97c6f71ffa4ad7743a723036fc21afb0c5aabd8a
Arvind <arvind@Arvinds-MacBook-Air.local>
2026-05-28 22:30:18 +0530

    refactor: updated frontend url for cors

--- backend/index.py
+++ backend/index.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", 'https://node-pipeline-editor-buildfrontend.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
